10f83182ea9d71bb1aaee1326c21c0416f78a699
Ubuntu <ubuntu@ip-172-26-80-118.ec2.internal>
2017-07-18 15:57:08 +0000

    renamed vars.yml to params.yml and updated ldap config

--- run-pipelines.sh
+++ run-pipelines.sh
@@ -2,9 +2,9 @@
 
 mkdir ~/concourse-pipelines/cf-mgt/temp
 
-spruce merge ~/concourse-pipelines/cf-mgt/vars.yml > ~/concourse-pipelines/cf-mgt/temp/vars.yml
+spruce merge ~/concourse-pipelines/cf-mgt/params.yml > ~/concourse-pipelines/cf-mgt/temp/params.yml
 
-fly -t sandbox set-pipeline -n -p cf-management --config ~/concourse-pipelines/cf-mgt/pipeline.yml --load-vars-from ~/concourse-pipelines/cf-mgt/temp/vars.yml
+fly -t sandbox set-pipeline -n -p cf-management --config ~/concourse-pipelines/cf-mgt/pipeline.yml --load-vars-from ~/concourse-pipelines/cf-mgt/temp/params.yml
 
 fly -t sandbox unpause-pipeline -p cf-management
 
